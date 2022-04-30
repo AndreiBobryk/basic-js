0ea274d348678d6ee45398749de8b6362dc099e0
Andrei Bobryk <andreibobryk1@gmail.com>
2022-04-30 18:00:44 +0300

    complete3

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,14 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(string) {
+  if ((typeof string !=='string') || (string === '') || (!parseInt(string)) ||  (+(string) <=0) || (+(string) >MODERN_ACTIVITY) ) {
+    return false;
+  }
+
+  const k = 0.693 / HALF_LIFE_PERIOD;
+  return Math.ceil(Math.log(MODERN_ACTIVITY/(+string)) / k)
+
 }
 
 module.exports = {
--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,10 +22,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+ function getDNSStats(domains) {
+  const arr = domains.slice('');
+  const obj = {};
+  for (let i=0; i<arr.length; i++) {
+    let elSum = ''
+   arr[i].split('.').reverse().forEach(el => {
+     el = '.' + el;
+     elSum += el;
+    
+ 
+     if (elSum in obj) {
+       obj[elSum] += 1;
+     } else {
+       obj[elSum] = 1;
+     }
+     
+   });
+ 
+  }
+ 
+ return obj;
+ 
+ 
+ }
 
 module.exports = {
   getDNSStats
